318e295a935ef022411e0a6202c5ea6721749800
Mohannad Hussain <mk@go2mk.com>
2023-10-05 16:40:30 -0400

    Added traceback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json, random, sys, argparse, os.path, time, datetime
+import json, random, sys, argparse, os.path, time, datetime, traceback
 from os import path
 
 STORESCU_PATH = '~/Apps/dcm4che-5.23.2/bin/storescu'
